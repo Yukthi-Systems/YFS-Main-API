223019130ef291d143a4145a61747d2547792008
Neko Nik <nikhil@nekonik.com>
2026-09-22 16:32:47 +0530

    Enhance folder deletion process: add current level tracking and detailed logging for recursive deletion of folder contents and file versions

--- src/handlers/deletion.rs
+++ src/handlers/deletion.rs
@@ -11,24 +11,27 @@ use uuid::Uuid;
 
 
 
-async fn delete_folder_contents_recursively(db_pool: &PgPool, owner_id: Uuid, folder_id: Uuid) -> Result<(), AppError> {
+async fn delete_folder_contents_recursively(db_pool: &PgPool, owner_id: Uuid, folder_id: Uuid, current_level: usize) -> Result<(), AppError> {
     // If there is nothing left in the folder, then delete the folder itself
     let is_empty = delete_folder_if_empty(&db_pool, &folder_id).await?;
     if is_empty {
         return Ok(());
     }
+    log::trace!("Current level {}: Finished checking if folder with ID: {} is empty", current_level, folder_id);
 
     let first_level_files = get_files_in_folder(&db_pool, &folder_id).await?;
     let first_level_subfolders = get_subfolders_in_folder(&db_pool, &folder_id).await?;
 
     // Delete all files in the current folder
     for file_id in first_level_files {
+        log::trace!("Current level {}: Deleting file with ID: {} in folder with ID: {}", current_level, file_id, folder_id);
         delete_file_and_versions(&db_pool, owner_id, file_id).await?;
     }
 
     // Recursively delete all subfolders
     for subfolder_id in first_level_subfolders {
-        Box::pin(delete_folder_contents_recursively(db_pool, owner_id, subfolder_id)).await?;
+        log::trace!("Current level {}: Recursively deleting subfolder with ID: {} in folder with ID: {}", current_level, subfolder_id, folder_id);
+        Box::pin(delete_folder_contents_recursively(db_pool, owner_id, subfolder_id, current_level + 1)).await?;
     }
 
     Ok(())
@@ -36,11 +39,16 @@ async fn delete_folder_contents_recursively(db_pool: &PgPool, owner_id: Uuid, fo
 
 
 pub async fn delete_folder_and_contents(db_pool: PgPool, owner_id: Uuid, folder_id: Uuid) -> Result<(), AppError> {
+    log::trace!("Deleting folder with ID: {} for owner ID: {}", folder_id, owner_id);
+
     // First Mark the folder as deleted in the database (soft delete)
     mark_folders_and_files_as_deleted(&db_pool, &[folder_id]).await?;
 
+    log::trace!("Marked folder with ID: {} as deleted for owner ID: {}", folder_id, owner_id);
+    log::trace!("Starting recursive deletion of contents for folder with ID: {} for owner ID: {}", folder_id, owner_id);
+
     // Loop through all files and subfolders within the folder and delete them recursively
-    delete_folder_contents_recursively(&db_pool, owner_id, folder_id).await?;
+    delete_folder_contents_recursively(&db_pool, owner_id, folder_id, 0).await?;
 
     Ok(())
 }
@@ -62,6 +70,8 @@ pub async fn delete_file_and_versions(db_pool: &PgPool, owner_id: Uuid, file_id:
             .or_default()
             .push(info);
     }
+
+    log::trace!("Deleting file with ID: {} and its versions: {}; Unique hosted locations: {}", file_id, file_info.len(), file_locations_map.len());
 
     // Delete based on the hosted server locations
     for (hosted_at, file_versions) in &file_locations_map {
@@ -88,6 +98,8 @@ pub async fn delete_file_and_versions(db_pool: &PgPool, owner_id: Uuid, file_id:
                 -total_deleted_count
             ).await?;
         }
+
+        log::trace!("Deleted file versions from host: {}; Total deleted size: {}; Total deleted count: {}", hosted_at, total_deleted_size, total_deleted_count);
     }
 
     Ok(())
@@ -107,6 +119,8 @@ pub async fn delete_file_version(db_pool: &PgPool, owner_id: Uuid, file_id: Uuid
         return Err(AppError::BadRequest("File version not found".into()));
     }
     let specific_version = specific_version.unwrap();
+
+    log::trace!("Deleting specific file version: {} for file ID: {} from host: {}", version, file_id, specific_version.hosted_at);
 
     // Delete from server
     delete_paths_from_server(
@@ -128,6 +142,11 @@ pub async fn delete_file_version(db_pool: &PgPool, owner_id: Uuid, file_id: Uuid
             -total_deleted_count
         ).await?;
     }
+
+    log::trace!(
+        "Deleted specific file version: {} for file ID: {} from host: {}; Total deleted size: {}; Total deleted count: {}",
+        version, file_id, specific_version.hosted_at, total_deleted_size, total_deleted_count
+    );
 
     Ok(())
 }
